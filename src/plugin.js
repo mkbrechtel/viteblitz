7dc7dc27c0fd37daf2e08592d680ca4f3126d27f
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2023-06-02 01:53:54 +0200

    remove typescript

--- src/plugin.js
+++ src/plugin.js
@@ -2,7 +2,21 @@
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
 
-const baseHTML = readFileSync(resolve(__dirname, 'base.html'), 'utf-8')
+const baseHTML = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Viteblitz</title>
+  </head>
+  <body>
+    <script type="module" src="/src/main.js"></script>
+    <div id="app"></div>
+  </body>
+</html>
+`
 
 const ViteBlitz = () => ({
     name: 'viteblitz',
