c7fc560ba504894253a46ab651cae03f8e39bd13
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2023-06-02 21:40:59 +0200

    add editor

--- src/Editor.vue
+++ src/Editor.vue
@@ -1,3 +1,79 @@
+<script setup lang="ts">
+  import { reactive, shallowRef, computed, onMounted } from 'vue'
+  import { javascript } from '@codemirror/lang-javascript'
+  import { html } from '@codemirror/lang-html'
+  import { json } from '@codemirror/lang-json'
+  import { markdown } from '@codemirror/lang-markdown'
+  import { oneDark } from '@codemirror/theme-one-dark'
+  import { Codemirror } from 'vue-codemirror'
+
+  const themes: any = { oneDark }
+  const languages: any = {
+    javascript: javascript(),
+    html: html(),
+    json: json(),
+    markdown: markdown()
+  }
+
+  const consoleLog = console.log
+  const code = shallowRef(`console.log('Hello World')`)
+  const view = shallowRef()
+  const config = reactive({
+    disabled: false,
+    indentWithTab: true,
+    tabSize: 4,
+    autofocus: true,
+    placeholder: 'input...',
+    language: 'javascript',
+    theme: 'one-dark',// 'default',
+    phrases: 'en-us'
+  })
+
+  const handleReady = (payload: any) => {
+    console.log('handleReady payload:', payload)
+  }
+
+  const extensions = computed(() => {
+    const result = []
+    result.push(languages[config.language])
+    if (themes[config.theme]) {
+      result.push(themes[config.theme])
+    }
+    return result
+  })
+
+  onMounted(() => {
+    console.log('mounted view:', view)
+  })
+
+</script>
+
 <template>
-  <div>Editor</div>
+      <codemirror
+        class="codemirror"
+        ref="cm"
+        :autofocus="config.autofocus"
+        :placeholder="config.placeholder"
+        :indentWithTab="config.indentWithTab"
+        :tabSize="config.tabSize"
+        :disabled="config.disabled"
+        :style="{
+          height: '100%',
+          backgroundColor: '#fff',
+          color: '#333'
+        }"
+        :extensions="extensions"
+        v-model="code"
+        @ready="handleReady"
+        @change="consoleLog('change', $event)"
+        @focus="consoleLog('focus', $event)"
+        @blur="consoleLog('blur', $event)"
+      />
 </template>
+
+<style scoped>
+  .codemirror {
+    height: 100%;
+    width: 100%;
+  }
+</style>
--- src/plugin.js
+++ src/plugin.js
@@ -1,30 +1,24 @@
-// read base.html from module directory
-import { readFileSync } from 'fs'
-import { resolve } from 'path'
-
 const baseHTML = `
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Viteblitz</title>
   </head>
   <body>
-    <script type="module" src="/src/main.js"></script>
+    <script type="module" src="/src/app.js"></script>
     <div id="app"></div>
   </body>
 </html>
 `
 
 const ViteBlitz = () => ({
-    name: 'viteblitz',
-    configureServer(server) {
-      server.middlewares.use('/__viteblitz',(req, res, next) => {
-          res.end(baseHTML);
-        });
-    },
-  })
+  name: 'viteblitz',
+  configureServer(server) {
+    server.middlewares.use('/__viteblitz',(req, res, next) => {
+        res.end(baseHTML);
+      });
+  },
+})
 
 export { ViteBlitz }
