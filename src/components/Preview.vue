40d598dc5eec42864a96a9dd97d9048f1b3152f9
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2023-06-04 17:34:06 +0200

    fix height of iframe

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -21,33 +21,27 @@ function previewLink() {
 </script>
 
 <template>
-    <v-toolbar density="compact">
-        <v-btn icon @click="previewGoBack">
-            <v-icon>mdi-arrow-left</v-icon>
-        </v-btn>
-        <v-btn icon @click="previewRefresh">
-            <v-icon>mdi-refresh</v-icon>
-        </v-btn>
-        <v-text-field
-            hide-details
-            single-line
-            v-model="url"
-            prepend-inner-icon="mdi-link"
-        ></v-text-field>
-        <v-btn icon @click="previewLink">
-            <v-icon>mdi-link-variant</v-icon>
-        </v-btn>
-        <v-btn icon>
-            <v-icon>mdi-layers-edit</v-icon>
-        </v-btn>
-    </v-toolbar>
-    <iframe ref="previewIframe" src="/"></iframe>
+    <v-content class="d-flex flex-column ma-0 pa-0 w-100 h-100">
+        <v-toolbar density="compact">
+            <v-btn icon @click="previewGoBack">
+                <v-icon>mdi-arrow-left</v-icon>
+            </v-btn>
+            <v-btn icon @click="previewRefresh">
+                <v-icon>mdi-refresh</v-icon>
+            </v-btn>
+            <v-text-field
+                hide-details
+                single-line
+                v-model="url"
+                prepend-inner-icon="mdi-link"
+            ></v-text-field>
+            <v-btn icon @click="previewLink">
+                <v-icon>mdi-link-variant</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>mdi-layers-edit</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <iframe ref="previewIframe" class="w-100 h-100" style="border:none;" :src="url"></iframe>
+    </v-content>
 </template>
-
-<style scoped>
-iframe {
-    border: none;
-    width: 100%;
-    height: 100%;
-}
-</style>
