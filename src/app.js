3f8170bbcfa090a5beb67cb270aee2cadeebf499
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2023-06-04 05:27:20 +0200

    add icon font to vuetify

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
 
+import '@mdi/font/css/materialdesignicons.css'
+
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 import App from './App.vue'
 
-const vuetify = createVuetify()
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+    },
+})
 
 createApp(App).use(vuetify).mount('#app')
