a5d6483fefca9c3a98e500ca32eb360655dfc718
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2023-06-04 02:27:46 +0200

    cleanup app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-
-const meta = import.meta
-
-const app = createApp(App)
-app.config.globalProperties.meta = import.meta
-
-app.mount('#app')
+createApp(App).mount('#app')
